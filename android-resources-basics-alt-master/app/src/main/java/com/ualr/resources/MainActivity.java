2a93c30656eff6b1bf821d6aa336380b62c9411f
watsonkalamari <klwatson3@ualr.edu>
2020-10-09 00:03:24 -0500

    Work in Progress
    Button still not changing colors when pressed.

--- android-resources-basics-alt-master/app/src/main/java/com/ualr/resources/MainActivity.java
+++ android-resources-basics-alt-master/app/src/main/java/com/ualr/resources/MainActivity.java
@@ -3,8 +3,11 @@ package com.ualr.resources;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.ualr.resources.databinding.ActivityMainBinding;
@@ -17,24 +20,32 @@ import com.google.android.material.snackbar.Snackbar;
 // TODO 5: Initialize the text property of the TextView element with the "country_description_text" id by using the value retrieved in the previous TODO point
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       ActivityMainBinding binding=ActivityMainBinding.inflate(getLayoutInflater());
+
+       final ActivityMainBinding binding=ActivityMainBinding.inflate(getLayoutInflater());
        setContentView(R.layout.activity_main);
         Resources res=this.getResources();
 
-        binding.countryDescriptionText.setText(res.getText(R.string.country_description).toString());
+        binding.countryDescriptionText.setText(res.getText(R.string.country_description));
+
         binding.lessonLearntButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
 
-
+                //Log.d(TAG,"Button has been pressed!");
+              
             }
         });
 
+
+
+        }
+
     }
 
-}
+
+
